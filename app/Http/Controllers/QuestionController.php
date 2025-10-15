6bde116c27d7d36ac838f9b8d438c7d4b4bbd864
dzikra24ti <dzikra24ti@mahasiswa.pcr.ac.id>
2025-10-15 21:51:42 +0700

    Praktikum5

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -28,25 +28,24 @@ class QuestionController extends Controller
     public function store(Request $request)
     {
       //dd($request->all());//
-        /*
-      $data ['nama'] = $request->nama;
+
+        $data ['nama'] = $request->nama;
         $data ['email'] = $request->email;
         $data ['pertanyaan']=$request->pertanyaan;
-        return view('home-question-respon',$data);*/
 
+
+        //return view('home-question-respon',$data);
 		$request->validate([
-		    'nama'  => 'required|max:10',
+            'nama'  => 'required|max:10',
 		    'email' => ['required','email'],
 		    'pertanyaan' => 'required|max:300|min:8',
         ],[
-
-        'nama.required'=>'Nama tidak boleh kosong',
-        'email.email'=>'Email tidak valid'
+            'nama.required'=>'Nama tidak boleh kosong',
+            'email.email'=>'Email tidak valid'
         ]);
-        $data['nama']       = $validatedData['nama'];
-         $data['email']      = $validatedData['email'];
-         $data['pertanyaan'] = $validatedData['pertanyaan'];
-		return view('home-question-respon',$data);
+        return redirect()->back()->with('info', 'Selamat, Kamu Lulus!');
+		//return view('home-question-respon',$data);
+
 }
 
     /**
