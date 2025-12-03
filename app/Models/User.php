533b060b20ba882a692d08a14200ccf8e537ce50
dzikra24ti <dzikra24ti@mahasiswa.pcr.ac.id>
2025-12-03 11:43:49 +0700

    Rumah

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles;
 
     protected $table = 'user';
     protected $primaryKey = 'id';
